b8b7eb7301f1443611fdc79f2975b6b22d0ddd3f
lettuce <58285036+lettuceboop@users.noreply.github.com>
2020-02-24 20:53:00 -0500

    get json from nvidia

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const fetch = require('node-fetch');
+let settings = { method: "Get" };
+const url = "https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json?JSON"
+
+app.get('/', function (req, res) {
+	fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        // do something with JSON
+		res.send(json)
+    });
+	
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
